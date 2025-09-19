890c0aea3df1dcb4c7b8ec11c121fca4e0b524b3
FullofMyself <wmaseko1@live.com>
2025-09-20 01:11:29 +0200

    Login Logic

--- routes/documents.js
+++ routes/documents.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const { authMiddleware } = require('../middleware/auth');
+const authMiddleware = require('../middleware/auth');
 
 // Allowed document types
 const DOC_TYPES = ['idCopy', 'payslip', 'proofOfResidence', 'bankStatement'];
